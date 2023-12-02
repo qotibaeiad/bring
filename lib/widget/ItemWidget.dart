3f377a8ddf286448b94eb7d0d1e0e94794a0c750
qotibamhamed <qotibaeiad98@gmail.com>
2023-12-02 21:19:22 +0200

    add shopping icon

--- lib/widget/ItemWidget.dart
+++ lib/widget/ItemWidget.dart
@@ -97,14 +97,31 @@ class ItemWidget extends StatelessWidget {
                     Padding(
                       padding: EdgeInsets.symmetric(vertical: 5),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Text(
                             "\$10",
                             style: TextStyle(
                               fontSize: 20,
                               fontWeight: FontWeight.bold,
+                              color: Colors.blue[900],
                             ),
-                          )
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Container(
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: Colors.blue[900],
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Icon(
+                                Icons.add_shopping_cart,
+                                color: Colors.white,
+                                size: 10,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                     ),
