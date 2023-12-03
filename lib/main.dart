cd4ab267862340771882463bc03931c1c4f939f8
qotibamhamed <qotibaeiad98@gmail.com>
2023-12-03 21:26:03 +0200

    remove unnecessary file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:bring/screen/Itemscreen.dart';
 import 'package:bring/screen/splashscreen.dart';
-import 'package:bring/widget/ItemWidget.dart';
 import 'package:flutter/material.dart';
 import 'server/socketserver.dart';
-import 'screen/homescreen.dart';
-import 'screen/messagescreen.dart';
 
 late SocketService socketService;
 
