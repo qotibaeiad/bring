6936d3b7e2de59dbc3290e54b5a3743fcf6c0ab3
qotibamhamed <qotibaeiad98@gmail.com>
2023-12-05 12:17:22 +0200

    final side sheet fix

--- lib/widget/BottomCartSheet.dart
+++ lib/widget/BottomCartSheet.dart
@@ -2,194 +2,171 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BottomCartSheet extends StatelessWidget {
-  const BottomCartSheet({super.key});
+  const BottomCartSheet({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: Colors
-                .transparent, // Set this to transparent to see the circular background
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0), // Adjust the radius as needed
-              topRight: Radius.circular(20.0), // Adjust the radius as needed
-            ),
-          ),
-          //padding: EdgeInsets.only(top: 20, bottom: 20),
-          //height: 338,
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height * 0.3,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      for (int i = 0; i < 8; i++)
-                        Container(
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 15),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 1,
-                                  blurRadius: 8,
-                                ),
-                              ]),
-                          child: Row(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.all(10),
-                                child: Image.asset(
-                                  'images/7.png',
-                                  height: 80,
-                                  width: 80,
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                for (int i = 0; i < 8; i++)
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 8,
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Image.asset(
+                            'images/7.png',
+                            height: 80,
+                            width: 80,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(bottom: 13),
+                              child: Text(
+                                'Item Title',
+                                style: TextStyle(
+                                  color: Colors.blue[900],
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                            ),
+                            Container(
+                              alignment: Alignment.center,
+                              child: Text(
+                                '\$20',
+                                style: TextStyle(
+                                  color: Colors.blue[900],
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Icon(
+                                Icons.disabled_by_default,
+                                color: Colors.blue[900],
+                                size: 30,
+                              ),
+                              SizedBox(
+                                height: 25,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
                                   Container(
-                                    margin: EdgeInsets.only(bottom: 13),
-                                    child: Text(
-                                      'Item Title',
-                                      style: TextStyle(
-                                          color: Colors.blue[900],
-                                          fontSize: 22,
-                                          fontWeight: FontWeight.bold),
+                                    padding: EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(20),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.5),
+                                          spreadRadius: 2,
+                                          blurRadius: 8,
+                                        ),
+                                      ],
+                                    ),
+                                    child: Icon(
+                                      CupertinoIcons.minus,
                                     ),
                                   ),
                                   Container(
-                                    alignment: Alignment.center,
+                                    margin:
+                                        EdgeInsets.symmetric(horizontal: 10),
                                     child: Text(
-                                      '\$20',
+                                      '02',
                                       style: TextStyle(
-                                          color: Colors.blue[900],
-                                          fontSize: 22,
-                                          fontWeight: FontWeight.bold),
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(20),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.5),
+                                          spreadRadius: 2,
+                                          blurRadius: 8,
+                                        ),
+                                      ],
+                                    ),
+                                    child: Icon(
+                                      CupertinoIcons.plus,
                                     ),
                                   ),
                                 ],
-                              ),
-                              Spacer(),
-                              Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 10),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    Icon(
-                                      Icons.disabled_by_default,
-                                      color: Colors.blue[900],
-                                      size: 30,
-                                    ),
-                                    SizedBox(
-                                      height: 25,
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Container(
-                                          padding: EdgeInsets.all(5),
-                                          decoration: BoxDecoration(
-                                            color: Colors.white,
-                                            borderRadius:
-                                                BorderRadius.circular(20),
-                                            boxShadow: [
-                                              BoxShadow(
-                                                color: Colors.grey
-                                                    .withOpacity(0.5),
-                                                spreadRadius: 2,
-                                                blurRadius: 8,
-                                              ),
-                                            ],
-                                          ),
-                                          child: Icon(
-                                            CupertinoIcons.minus,
-                                          ),
-                                        ),
-                                        Container(
-                                          margin: EdgeInsets.symmetric(
-                                              horizontal: 10),
-                                          child: Text(
-                                            '02',
-                                            style: TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                        ),
-                                        Container(
-                                          padding: EdgeInsets.all(5),
-                                          decoration: BoxDecoration(
-                                            color: Colors.white,
-                                            borderRadius:
-                                                BorderRadius.circular(20),
-                                            boxShadow: [
-                                              BoxShadow(
-                                                color: Colors.grey
-                                                    .withOpacity(0.5),
-                                                spreadRadius: 2,
-                                                blurRadius: 8,
-                                              ),
-                                            ],
-                                          ),
-                                          child: Icon(
-                                            CupertinoIcons.plus,
-                                          ),
-                                        )
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              ),
+                              )
                             ],
                           ),
                         ),
-                      ///////////////////////
-                    ],
-                  ),
-                ),
-              ),
-
-              /////////////////////
-              ///
-              ///
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                padding: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 1,
-                        blurRadius: 8,
-                      ),
-                    ]),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          'Delivery',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue[900],
-                          ),
-                        ),
                       ],
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                // Additional Widgets can be added here
+              ],
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+          padding: EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 8,
               ),
+            ],
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Delivery',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[900],
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
