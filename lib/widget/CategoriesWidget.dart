86b46e9814627ae8334fb897170962868a5a1c69
qotibamhamed <qotibaeiad98@gmail.com>
2023-12-07 21:33:40 +0200

    fix stream

--- lib/widget/CategoriesWidget.dart
+++ lib/widget/CategoriesWidget.dart
@@ -19,12 +19,11 @@ class _CategoriesWidgetState extends State<CategoriesWidget> {
     _dataStreamController = StreamController<List<Item>>.broadcast();
 
     socketService.socket.on('streamitems', (data) {
-      print('Received data: $data');
-      // Parse the data into Item object
       Item item = Item.fromJson(data);
+      print(item);
       setState(() {
-        // Update the items list
         dataList.add(item);
+        _dataStreamController.add(dataList);
       });
     });
   }
@@ -60,77 +59,49 @@ class _CategoriesWidgetState extends State<CategoriesWidget> {
         SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(
-            children: [
-              StreamBuilder<List<Item>>(
-                stream: _dataStreamController.stream,
-                builder: (context, snapshot) {
-                  print("Connection State: ${snapshot.connectionState}");
-                  if (snapshot.connectionState == ConnectionState.waiting &&
-                      !snapshot.hasData) {
-                    return CircularProgressIndicator();
-                  } else if (snapshot.hasData) {
-                    List<Item> data = snapshot.data!;
-                    // Build your UI using the data
-                    return Container(
+            children: dataList
+                .map((item) => Container(
+                      padding: const EdgeInsets.all(8.0),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
                       height: 50,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: data.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              margin: EdgeInsets.symmetric(
-                                horizontal: 10,
-                                vertical: 5,
-                              ),
-                              height: 50,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: Colors.white,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey.withOpacity(0.7),
-                                    spreadRadius: 1,
-                                    blurRadius: 5,
-                                  ),
-                                ],
-                              ),
-                              child: Row(
-                                children: [
-                                  customContainer(
-                                    path: data[index].url,
-                                    radius: 20,
-                                    elevation: 2,
-                                    width: 50,
-                                    height: 50,
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(right: 5),
-                                    child: Text(
-                                      data[index].category,
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  )
-                                ],
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.7),
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          customContainer(
+                            path:
+                                item.url, // assuming Item has a 'url' property
+                            radius: 20,
+                            elevation: 2,
+                            width: 50,
+                            height: 50,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 5),
+                            child: Text(
+                              item.category, // assuming Item has a 'category' property
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          );
-                        },
+                          )
+                        ],
                       ),
-                    );
-                  } else if (snapshot.hasError) {
-                    print("Error: ${snapshot.error}");
-                    return Text('Error: ${snapshot.error}');
-                  } else {
-                    return Text('Waiting for data...');
-                  }
-                },
-              ),
-            ],
+                    ))
+                .toList(),
           ),
         ),
       ],
